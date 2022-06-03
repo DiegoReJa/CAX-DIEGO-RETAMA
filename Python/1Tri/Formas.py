deb5bad6097ac5fa7cccbd9c5543044e612c1b21
DiegoReJa <104397661+DiegoReJa@users.noreply.github.com>
2022-06-03 14:13:48 -0500

    Add files via upload

---
+++ Python/1Tri/Formas.py
@@ -0,0 +1,19 @@
+print('Saca el area de un rectangulo')
+Lado1=int(input('lado 1: '))
+Lado2=int(input('lado 2: '))
+R=Lado1*Lado2
+print('el resultado es',R)
+
+print('Saca el area de un triangulo')
+b=int(input('Base 1: '))
+H=int(input('Altura 1: '))
+A=b*H
+A2=A/2
+print('el resultado es',A2)
+
+print('Saca el area de un pentagono')
+a=int(input('apotema: '))
+p=int(input('perimetro: '))
+A=a*b
+A2=A/2
+print('el resulatdo es',A2)
