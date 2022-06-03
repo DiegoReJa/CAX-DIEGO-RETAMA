c9e4e4e51d101baea3f52b358cec62253c5c7a3b
DiegoReJa <104397661+DiegoReJa@users.noreply.github.com>
2022-06-03 14:10:02 -0500

    Add files via upload

---
+++ Python/1Tri/DigitalC.py
@@ -0,0 +1,12 @@
+question="how can we act with empathy and positivy we're online?"
+print(question,"not be rude and be nicer to others")
+
+Opinion="la chica se comporto muy infantil al hacer eso y postearlo en internet mientras que el chico hablaba enserio"
+print(Opinion)
+
+identify="el chico y la chica,el ciberbullying"
+feel="el chico se siente triste y la chica penso que era divertido"
+imagine1="el chico pudo tomar todo de broma"
+imagine2="la chica no subia esa foto humillando al chico"
+imagine3="la humillacion al chico pudo llegar a peor hasta tal punto de acabar con su vida"
+say="muy mal"
