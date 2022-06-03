a3671db8f26bcb259eea7d8e2e395329cf41faa9
DiegoReJa <104397661+DiegoReJa@users.noreply.github.com>
2022-06-03 18:32:53 -0500

    Add files via upload

---
+++ Python/1Tri/Grupo.py
@@ -0,0 +1,14 @@
+print("reprobados")
+
+grupo="c"
+
+print("reprobado esta",grupo)
+print("este grupo esta rifado, es el",grupo)
+
+print("escribe tu nombre: ")
+nombre=input()
+print('saludos',nombre)
+
+
+escuela=input('escribe tu escuela: ')
+print('yo tambien estudio en el ',escuela)
