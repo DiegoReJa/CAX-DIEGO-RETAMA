15d13d43bc7048fd9c1928beb2df83faa455ca4a
DiegoReJa <104397661+DiegoReJa@users.noreply.github.com>
2022-07-01 13:35:14 -0500

    Add files via upload

---
+++ Python/2tri/Nombre.py
@@ -0,0 +1,15 @@
+print('escribe tu nombre: ')
+nombre=input()
+print('un gusto',nombre)
+
+comidafavorita=input('escribe tu comida favorita: ')
+print('que buenos gustos tienes')
+
+grupo=input('escribe tu grupo: ')
+print('reprobado esta el', grupo)
+
+escuela=input('escribe el nombre de tu escuela:  ')
+print('yo tambien estudio en el',escuela)
+
+extra=input('escribe tu edad: ')
+print('que joven')
---
+++ Python/2tri/Numeros .py
@@ -0,0 +1,28 @@
+numero=int (input("ingresa un numero: "))
+ 
+resultado=numero*2
+print('el resultado es: ', resultado)
+
+
+
+#classcode: preparen una claculadora de dos numeros,debera sumar,restar,multiplicar y dividir 
+
+numero=int(input('ingresa un numero: '))
+suma=int(input('ingresa un numero para sumar:  '))
+resultado=numero+suma
+print('el resultado es: ',resultado)
+
+numero=int(input('ingresa un numero: '))
+resta=int(input('ingresa un numero para restar: '))
+resultado=numero-resta
+print('el resultado es:',resultado)
+
+numero=int(input('ingresa un numero:  '))
+mult=int(input('ingresa un numero para multiplicar: '))
+resultado=numero*mult
+print('el resultado es:',resultado)
+
+numero=int(input('ingresa un numero: '))
+division=int(input('ingresa un numero para dividir: '))
+resultado=numero/5
+print('el resultado es:',resultado)
